b307d4264d500597ccb8a78902b667919771e4c5
evkgithub <evijayakumar@gmail.com>
2013-11-10 15:04:07 -0800

    First Commit
    First Commit to Hug

---
+++ src/com/learnself/git/core/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.learnself.git.core;
+
+/**
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
