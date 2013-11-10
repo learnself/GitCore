1fc803a923882b60577d6cf8721097207715bcfc
evkgithub <evijayakumar@gmail.com>
2013-11-10 15:13:20 -0800

    Second Commit
    Second Commit

---
+++ src/com/learnself/git/core/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.learnself.git.core;
+
+/**
+ * @author VijayKumar_E
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Cheese!...");
+	}
+
+}
